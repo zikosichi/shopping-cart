d508265c18492ffc961c878467e22369c3857254
Zviad Sichinava <zviadsichinava@gmail.com>
2017-01-12 23:08:12 +0400

    Product details page styling & animations

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -11,12 +11,19 @@ export class ProductDetailsComponent implements OnInit {
   private id: number;
   private sub: any;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
-       this.id = +params['id']; // (+) converts string 'id' to a number
+      this.id = +params['id']; // (+) converts string 'id' to a number
     });
+
+    TweenMax.to(".pd-details", 0.8, { ease: Power3.easeOut, y: 0 });
+    TweenMax.to(".pd-photos", 0.8, { ease: Power3.easeOut, y: 0 });
+    TweenMax.from(".form", 1.2, { delay: 0.3, ease: Power3.easeOut, opacity: 0, y: 100 });
+    // TweenMax.from(".pd-photos-content", 1.2, { delay: 0.3, ease: Power3.easeOut, y: -100 });
+    TweenMax.to(".continue-shopping", 0.8, { ease: Power3.easeOut, delay: 0.8, "min-width": "150px" });
+
   }
 
   ngOnDestroy() {
